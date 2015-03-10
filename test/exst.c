dcffb70547b8cf118d57aee47ca1a0d3e4b0ccd9
RyanBeatty <rvbeatty@email.wm.edu>
2015-03-10 01:47:07 -0400

    added exstensible file test. untested

---
+++ test/exst.c
@@ -0,0 +1,45 @@
+
+
+#define N 100
+#define INFILE "toobig2"
+#define OUTFILE "res.txt"
+#include "syscall.h"
+
+void clear(char *b) {
+	int i = 0;
+	for(; i < N; ++i)
+		buffer[i] = '\0';
+}
+
+int main() {
+	int fdin;
+	int fdout;
+	int read;
+	char buffer[N];
+
+	Create(OUTFILE);
+	
+	fdin = Open(INFILE);
+	if(fdin < 0) {
+		Write("error: could not open toobig2\n", sizeof("error: could not open toobig2\n"), 1);
+		Exit(1);
+	}
+
+	fdin = Open(OUTFILE);
+	if(fdin < 0) {
+		Write("error: could not open res.txt\n", sizeof("error: could not open res.txt\n"), 1);
+		Close(fdin);
+		Exit(1);
+	}
+
+	clear(buffer);
+	while((read = Read(buffer, N, fdin)) > 0) {
+		Write(buffer, read, fdout);
+		clear(buffer);
+	}
+
+	Close(fdin);
+	Close(fdout);
+	Write("finished copy\n", sizeof("finished copy\n"), 1);
+	Exit(0);
+}
