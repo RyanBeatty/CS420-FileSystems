2d35e1a70d614f7a1d8c48c5a0d6192872c1784b
Ryan Beatty <rvbeatty@email.wm.edu>
2015-03-10 01:33:21 -0400

    fstest1.c test passes

--- test/fstest1.c
+++ test/fstest1.c
@@ -3,13 +3,14 @@
 int main() {
 	int fd;
 
-	Create("fstest1.txt");
-	fd = Open("fstest1.txt");
+	Create("t1.txt");
+	fd = Open("t1.txt");
 	if(fd < 0) {
 		Write("error: could not open file\n", sizeof("error: could not open file\n"), 1);
 		Exit(1);
 	}
 	Write("hello world\n", sizeof("hello world\n"), fd);
 	Close(fd);
+	Write("finished\n", sizeof("finished\n"), 1);
 	Exit(0);
 }
