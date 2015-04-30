a90e50b6748be85f083e078167fc30933c0f8a53
RyanBeatty <rvbeatty@email.wm.edu>
2015-04-30 16:12:27 -0400

    fixed makedir test

--- test/makedir.c
+++ test/makedir.c
@@ -7,11 +7,12 @@ and specify relative paths for creating files
 
 int main() {
 	MakeDir("test");
-	Create("foo");
+	Create("test/foo");
 	if(Open("test/foo") < 0) {
 		Write("error: file open failed\n", sizeof("error: file open failed\n"), 1);
 		Exit(1);
 	}
 
+	Write("test passed\n", sizeof("test passed\n"), 1);
 	Exit(0);
 }
