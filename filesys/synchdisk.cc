4a02b8e6643ff9325813794cf4c4a4563b7d0c61
Ryan Beatty <rvbeatty@email.wm.edu>
2015-04-30 21:53:03 -0400

    added cacheentry and cache classes. untested

--- filesys/synchdisk.cc
+++ filesys/synchdisk.cc
@@ -1,3 +1,4 @@
+#ifdef CHANGED
 // synchdisk.cc 
 //	Routines to synchronously access the disk.  The physical disk 
 //	is an asynchronous device (disk requests return immediately, and
@@ -17,6 +18,68 @@
 #include "copyright.h"
 #include "synchdisk.h"
 #include <new>
+
+#define CACHE_SIZE 10
+
+void SectorCopy(char *dst, char *src) {
+    for(int i = 0; i < SectorSize; ++i)
+        dst[i] = src[i];
+}
+
+
+class CacheEntry {
+public:
+    bool valid;
+    int sector;
+    char block[SectorSize];
+
+    CacheEntry() {
+        valid = false;
+        sector = -1;
+        bzero(block, SectorSize);
+    }
+};
+
+class Cache {
+public:
+    CacheEntry entries[CACHE_SIZE];
+
+    Cache() {
+        for(int i = 0; i < CACHE_SIZE; ++i)
+            entries[i].valid = false;
+    }
+
+    bool inCache(int sector) {
+        for(int i = 0; i < CACHE_SIZE; ++i) {
+            if(entries[i].valid && entries[i].sector == sector)
+                return true;
+        }
+        return false;
+    }
+
+    void Add(char *data, int sector) {
+        for(int i = 0; i < CACHE_SIZE; ++i) {
+            if(!entries[i].valid) {
+                entries[i].valid = true;
+                entries[i].sector = sector;
+                SectorCopy(entries[i].block, data);
+                return ;
+            }
+        }
+
+        int victim = Random() % CACHE_SIZE;
+        entries[victim].valid = false;
+    }
+
+    void Delete(int sector) {
+        for(int i = 0; i < CACHE_SIZE; ++i) {
+            if(entries[i].sector == sector) {
+                entries[i].valid = false;
+                return ;
+            }
+        }
+    }
+};
 
 //----------------------------------------------------------------------
 // DiskRequestDone
@@ -108,3 +171,132 @@ SynchDisk::RequestDone()
 { 
     semaphore->V();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#else
+
+// synchdisk.cc 
+//  Routines to synchronously access the disk.  The physical disk 
+//  is an asynchronous device (disk requests return immediately, and
+//  an interrupt happens later on).  This is a layer on top of
+//  the disk providing a synchronous interface (requests wait until
+//  the request completes).
+//
+//  Use a semaphore to synchronize the interrupt handlers with the
+//  pending requests.  And, because the physical disk can only
+//  handle one operation at a time, use a lock to enforce mutual
+//  exclusion.
+//
+// Copyright (c) 1992-1993 The Regents of the University of California.
+// All rights reserved.  See copyright.h for copyright notice and limitation 
+// of liability and disclaimer of warranty provisions.
+
+#include "copyright.h"
+#include "synchdisk.h"
+#include <new>
+
+//----------------------------------------------------------------------
+// DiskRequestDone
+//  Disk interrupt handler.  Need this to be a C routine, because 
+//  C++ can't handle pointers to member functions.
+//----------------------------------------------------------------------
+
+static void
+DiskRequestDone (int arg)
+{
+    SynchDisk* disk = (SynchDisk *)arg;
+
+    disk->RequestDone();
+}
+
+//----------------------------------------------------------------------
+// SynchDisk::SynchDisk
+//  Initialize the synchronous interface to the physical disk, in turn
+//  initializing the physical disk.
+//
+//  "name" -- UNIX file name to be used as storage for the disk data
+//     (usually, "DISK")
+//----------------------------------------------------------------------
+
+SynchDisk::SynchDisk(char* name)
+{
+    semaphore = new(std::nothrow) Semaphore("synch disk", 0);
+    lock = new(std::nothrow) Lock("synch disk lock");
+    disk = new(std::nothrow) Disk(name, DiskRequestDone, (int) this);
+}
+
+//----------------------------------------------------------------------
+// SynchDisk::~SynchDisk
+//  De-allocate data structures needed for the synchronous disk
+//  abstraction.
+//----------------------------------------------------------------------
+
+SynchDisk::~SynchDisk()
+{
+    delete disk;
+    delete lock;
+    delete semaphore;
+}
+
+//----------------------------------------------------------------------
+// SynchDisk::ReadSector
+//  Read the contents of a disk sector into a buffer.  Return only
+//  after the data has been read.
+//
+//  "sectorNumber" -- the disk sector to read
+//  "data" -- the buffer to hold the contents of the disk sector
+//----------------------------------------------------------------------
+
+void
+SynchDisk::ReadSector(int sectorNumber, char* data)
+{
+    lock->Acquire();            // only one disk I/O at a time
+    disk->ReadRequest(sectorNumber, data);
+    semaphore->P();         // wait for interrupt
+    lock->Release();
+}
+
+//----------------------------------------------------------------------
+// SynchDisk::WriteSector
+//  Write the contents of a buffer into a disk sector.  Return only
+//  after the data has been written.
+//
+//  "sectorNumber" -- the disk sector to be written
+//  "data" -- the new contents of the disk sector
+//----------------------------------------------------------------------
+
+void
+SynchDisk::WriteSector(int sectorNumber, char* data)
+{
+    lock->Acquire();            // only one disk I/O at a time
+    disk->WriteRequest(sectorNumber, data);
+    semaphore->P();         // wait for interrupt
+    lock->Release();
+}
+
+//----------------------------------------------------------------------
+// SynchDisk::RequestDone
+//  Disk interrupt handler.  Wake up any thread waiting for the disk
+//  request to finish.
+//----------------------------------------------------------------------
+
+void
+SynchDisk::RequestDone()
+{ 
+    semaphore->V();
+}
+#endif
